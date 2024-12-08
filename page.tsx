64416c25a495a2472d71e93992112867f8ccbb4c
sumaira3015 <145383710+sumaira3015@users.noreply.github.com>
2024-12-08 23:45:20 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,22 +1,60 @@
-import Category from "@/components/category";
-import Footer from "@/components/footer";
-import Header from "@/components/header";
-import HeaderBottom from "@/components/headerBottom";
-import HeaderLast from "@/components/headerLast";
-import Hero from "@/components/hero";
-import LogoFeature from "@/components/logoFeature";
-import OurProduct from "@/components/ourProduct";
-import Image from "next/image";
-import { BiCategory } from "react-icons/bi";
+import React from 'react'
+import Image from "next/image"
+import { Button } from '@/components/ui/button'
+import { FiShoppingCart } from "react-icons/fi";
+import Feature from '@/components/feature';
 
-export default function Home() {
+
+function Page() {
   return (
-<>
-<Hero/>
-<LogoFeature/>
-<Category/>
-<OurProduct/>
+    <>
+       <section>
 
-</>
-  );
+        {/* first div */}
+        <div className='flex gap-[111px]'>
+            <div>
+                <Image src={"/products/product2.png"} alt="chairi" width={675} height={607}/>
+            </div>
+
+            {/* text */}
+            <div>
+                <h1 className='text-[60px] leading-[66px] font-bold text-[#272343]'>Library Stool <br/> Chair</h1>
+
+                <Button className='text-[20] leading-[22] font-semibold text-white bg-[#029FAE] mt-[32px] rounded-[100px]'>$20.00 USD</Button>
+
+                <div className='w-[400px] text-[22] leading-[33px] text-[#272343] pt-[37px] border-t-[1px] mt-[37px]'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt erat enim. Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+                </div>
+
+                <Button className='w-[212px] h-[63px] flex items-center gap-[9px] text-[#FFFFFF] bg-[#029FAE] mt-[32px]'>
+                    <FiShoppingCart size={29} color='white'/>
+                    <p className='text-[20px] leading-[22px] font-medium '>Add To cart</p>
+                </Button>
+            </div>
+        </div>
+
+
+        {/* feature products */}
+        <div className='w-full mt-[123px]'>
+
+            <div className='text-[#000000] font-bold flex items-center justify-between'>
+                <h1 className='text-[28px] leading-[33px]  tracking-[20%]'>Featured Products</h1>
+
+                <p className='text-[18px] leading-[21px] underline '>View all</p>
+            </div>
+
+
+            {/* main div */}
+            <div className='w-full h-[306px] flex gap-[26px] mt-[69px]'>
+                <Feature/>
+            </div>
+
+
+        </div>
+
+       </section>
+    </>
+  )
 }
+
+export default Page
